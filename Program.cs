39c4a018e93e9fe17f0bcdc1a9435dd0bfb4403e
“ShubhamvShrikhande” <shubhamvshri99@gmail.com>
2023-02-12 21:35:29 +0530

    UC4- Added

--- Program.cs
+++ Program.cs
@@ -4,13 +4,17 @@
     {
         static void Main(string[] args)
         {
-
-
             Queue queue = new Queue();
             queue.Enqueue(56);
             queue.Enqueue(30);
             queue.Enqueue(70);
             queue.display();
+            queue.Dequeue();
+            queue.display();
+            queue.Dequeue();
+            queue.display();
+            queue.Dequeue();
+            queue.display(); ;
         }
     }
 }
